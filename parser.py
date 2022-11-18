bf0dd4e994e9160cfc0b13020b60898f9617fa5d
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2022-11-18 23:10:44 +0530

    Part 9

--- parser.py
+++ parser.py
@@ -1,38 +1,40 @@
-import toen
 import lexer
 
-#Int,Float,String;
-INTEGER = 'INTEGER'
-FLOAT = 'FLOAT'
-STRING = 'STRING'
-
-PLUS = 'PLUS'
-MINUS = 'MINUS'
-MULTIPLY = 'MULTIPLY'
-DIVIDE = 'DIVIDE'
-MODULO = 'MODULO'
-
-LPAREN = '('
-RPAREN = ')'
-LCURL = '{'
-RCURL = '}'
-
-EQUAL = '='
-SEMICOLON    = ';'
-ID = 'ID'
-COMMA = ','
-DOT = '.'
-
-COMMENT = '//'
-
-WHILE = 'while'
-IF = 'if'
-ELSE = 'else'
-ELSEIF = 'elseif'
-LET = 'let'
-VAR = 'var'
-
-EOF = 'EOF'
+INTEGER       = 'INTEGER'
+REAL          = 'REAL'
+INTEGER_CONST = 'INTEGER_CONST'
+REAL_CONST    = 'REAL_CONST'
+PLUS          = 'PLUS'
+MINUS         = 'MINUS'
+MUL           = 'MUL'
+INTEGER_DIV   = 'INTEGER_DIV'
+FLOAT_DIV     = 'FLOAT_DIV'
+LPAREN        = 'LPAREN'
+RPAREN        = 'RPAREN'
+ID            = 'ID'
+ASSIGN        = 'ASSIGN'
+BEGIN         = 'BEGIN'
+END           = 'END'
+SEMI          = 'SEMI'
+DOT           = 'DOT'
+PROGRAM       = 'PROGRAM'
+VAR           = 'VAR'
+COLON         = 'COLON'
+COMMA         = 'COMMA'
+PROCEDURE     = 'PROCEDURE'
+EOF           = 'EOF'
+#NEW
+SYMBOL_SLASH  ='SYMBOL_SLASH'
+LET           ='LET'
+IF            ='IF'
+ELSE          ='ELSE'
+WHILE         ='WHILE'
+SWITCH        ='SWITCH'
+CASE          ='CASE'
+STRING        ='STRING'
+STRING_       ='STRING_'
+LCURL         ='LCURL'
+RCURL         ='RCURL'
 
 class AST(object):
     pass
@@ -54,14 +56,37 @@ class Num(AST):
         self.token = token
         self.value = token.value
 
+class Compound(AST):
+    """Represents a '{ ... }' block"""
+    def __init__(self):
+        self.children = []
+
+
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+
+class Var(AST):
+    """The Var node is constructed out of ID token."""
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+class NoOp(AST):
+    pass
+
 
 class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
         # set current token to the first token taken from the input
-        self.current_token = self.lexer.getNextToken()
+        self.current_token = self.lexer.get_next_token()
 
     def error(self):
+        #print(self.current_token)
         raise Exception('Invalid syntax')
 
     def eat(self, token_type):
@@ -70,12 +95,90 @@ class Parser(object):
         # and assign the next token to the self.current_token,
         # otherwise raise an exception.
         if self.current_token.type == token_type:
-            self.current_token = self.lexer.getNextToken()
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
 
+    def program(self):
+        """program : compound_statement DOT"""
+        node = self.compound_statement()
+        return node
+
+    def compound_statement(self):
+        """
+        compound_statement: { statement_list }
+        """
+        nodes = self.statement_list()
+
+        root = Compound()
+        for node in nodes:
+            root.children.append(node)
+
+        return root
+
+    def statement_list(self):
+        """
+        statement_list : statement
+                       | statement SEMI statement_list
+        """
+        node = self.statement()
+
+        results = [node]
+
+        while self.current_token.type == SEMI:
+            self.eat(SEMI)
+            results.append(self.statement())
+
+        if self.current_token.type == ID:
+            self.error()
+
+        return results
+
+    def statement(self):
+        """
+        statement : compound_statement
+                  | assignment_statement
+                  | empty
+        """
+        """elif self.current_token.type == VAR:
+            node = self.assignment_statement()"""
+        if self.current_token.type in (VAR, LET):
+            self.eat(self.current_token.type)
+            self.lexer.advance()
+            node = self.assignment_statement()
+        else:
+            node = self.empty()
+        return node
+
+    def assignment_statement(self):
+        """
+        assignment_statement : variable ASSIGN expr
+        """
+        
+        left = self.variable()
+        print(left.value)
+        token = self.current_token
+        self.eat(ASSIGN)
+        right = self.expr()
+        print(right.value)
+        node = Assign(left, token, right)
+        return node
+
+    def variable(self):
+        """
+        variable : ID
+        """
+        node = Var(self.current_token)
+        self.eat(ID)
+        return node
+
+    def empty(self):
+        """An empty production"""
+        return NoOp()
+
+
     def factor(self):
-        """factor : INTEGER | LPAREN expr RPAREN"""
+        """factor : Variable|Unary Op(Factor) |INTEGER| LPAREN expr RPAREN"""
         token = self.current_token
         if token.type == PLUS:
             self.eat(PLUS)
@@ -85,25 +188,28 @@ class Parser(object):
             self.eat(MINUS)
             node = UnaryOp(token, self.factor())
             return node
-        elif token.type == INTEGER:
-            self.eat(INTEGER)
+        elif token.type == INTEGER_CONST:
+            self.eat(INTEGER_CONST)
             return Num(token)
         elif token.type == LPAREN:
             self.eat(LPAREN)
             node = self.expr()
             self.eat(RPAREN)
             return node
+        else:
+            node = self.variable()
+            return node
 
     def term(self):
         """term : factor ((MUL | DIV) factor)*"""
         node = self.factor()
 
-        while self.current_token.type in (MULTIPLY, DIVIDE):
+        while self.current_token.type in (MUL, FLOAT_DIV):
             token = self.current_token
-            if token.type == MULTIPLY:
-                self.eat(MULTIPLY)
-            elif token.type == DIVIDE:
-                self.eat(DIVIDE)
+            if token.type == MUL:
+                self.eat(MUL)
+            elif token.type == FLOAT_DIV:
+                self.eat(FLOAT_DIV)
 
             node = BinOp(left=node, op=token, right=self.factor())
 
@@ -129,5 +235,8 @@ class Parser(object):
         return node
 
     def parse(self):
-        return self.expr()
+        node = self.program()
+        if self.current_token.type != EOF:
+            self.error()
 
+        return node
